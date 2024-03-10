ecc6c18ffc2df81c7cba301b62fa98285aa88e06
youkwhd <lolywk@tutanota.com>
2024-03-10 14:55:31 +0700

    chore: draw entire board

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "board.h"
 
 #define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 450
+#define WINDOW_HEIGHT 440
 
 int main(void)
 {
@@ -16,24 +16,30 @@ int main(void)
     SetTargetFPS(60);
 
     int n_cell = (WINDOW_WIDTH - 20) / CELL_WIDTH;
+    int board_height = (WINDOW_HEIGHT - 20) / CELL_HEIGHT;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(BLACK);
 
-        for (int i = 0; i < n_cell; i++) {
-            int posx = (i * CELL_WIDTH) + 10;
-            int endposx = posx + CELL_WIDTH;
-            DrawLine(posx, 10, endposx, 10, RAYWHITE);
-            DrawLine(posx, 10, posx, 10 + CELL_HEIGHT, RAYWHITE);
-            DrawLine(posx + CELL_WIDTH, 10, posx + CELL_WIDTH, 10 + CELL_HEIGHT, RAYWHITE);
-            DrawLine(posx, 10 + CELL_WIDTH, posx + CELL_WIDTH, 10 + CELL_WIDTH, RAYWHITE);
+        for (int i = 0; i < board_height; i++) {
+            int posy = (i * CELL_HEIGHT) + 10;
+
+            for (int j = 0; j < n_cell; j++) {
+                int posx = (j * CELL_WIDTH) + 10;
+
+                DrawLine(posx, posy, posx + CELL_WIDTH, posy, RAYWHITE);
+                DrawLine(posx, posy, posx, posy + CELL_HEIGHT, RAYWHITE);
+                DrawLine(posx + CELL_WIDTH, posy, posx + CELL_WIDTH, posy + CELL_HEIGHT, RAYWHITE);
+                DrawLine(posx, posy + CELL_WIDTH, posx + CELL_WIDTH, posy + CELL_WIDTH, RAYWHITE);
+            }
         }
 
         EndDrawing();
     }
 
+    std::cout << board_height << std::endl;
     CloseWindow();
     return 0;
 }
